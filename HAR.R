5a96bdd749e20f7755f93efc8eee29025808f310
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-09-17 16:08:01 +1000

    gbm

--- HAR.R
+++ HAR.R
@@ -12,12 +12,20 @@ na_train <- sapply(1:length(names(train)), function(i, na_base=c()){
     na_col <- mean(is.na(train[i]))
     na_base <- c(na_base, na_col)})
 names(na_train) <- names(train)
-na_train <- sort(na_train,decreasing = T) 
-train2 <- na.omit(train)
+na_train <- sort(na_train,decreasing = T)
+na_index <- na_train > .5
+train2 <- train[,-na_index]
 # non zero variable
-nzv <- nearZeroVar(train2,saveMetrics = T)
-nzv
+nzv <- nearZeroVar(train2,saveMetrics = F)
 training <- train2[,-nzv]
 dim(train2)
-head(training)
+dim(training)
 # split train and test
+
+# run the model
+set.seed(888)
+fitControl <- trainControl(method = "repeatedcv", number = 10, repeats = 10)
+gbmGrid <-  expand.grid(interaction.depth = c(1, 5, 9), n.trees = (1:30)*50, shrinkage = 0.1)
+fit<- train(classe~., data=training, method = 'gbm', trControl=fitControl, tuneGrid = gbmGrid)
+pred <- predict(fit, training)
+result <- confusionMatrix(pred, training$classe)
