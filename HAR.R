beda1eb7b9850fa18a6fc5d2363cc6a3de138bc1
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-09-17 15:36:09 +1000

    data exploration

--- HAR.R
+++ HAR.R
@@ -1,16 +1,22 @@
 # setup and load data
 setwd("/Users/ivan/Work_directory/Human-Activity-Recognition/")
 library(caret)
-train <- read.csv('data/pml-training.csv')
+train <- read.csv('data/pml-training.csv', stringsAsFactor=F)
 # explore data
 summary(train)
 names(train)
 str(train)
+dim(train)
 # NA percents of each variable
 na_train <- sapply(1:length(names(train)), function(i, na_base=c()){
     na_col <- mean(is.na(train[i]))
     na_base <- c(na_base, na_col)})
 names(na_train) <- names(train)
 na_train <- sort(na_train,decreasing = T) 
-na_train
-# data imputation
+train2 <- na.omit(train)
+# non zero variable
+nzv <- nearZeroVar(train2,saveMetrics = T,uniqueCut = 1)
+nzv[1:10,]
+training <- train[,-nzv]
+head(training)
+# split train and test
